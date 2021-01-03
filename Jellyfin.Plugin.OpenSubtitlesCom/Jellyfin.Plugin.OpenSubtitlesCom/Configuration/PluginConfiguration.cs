422c2eefa8dd4d7209b3e973aa18a120ca1e455e
AliceDTRH <57547425+AliceDTRH@users.noreply.github.com>
2021-01-03 18:31:14 +0100

    Fixes #3

--- Jellyfin.Plugin.OpenSubtitlesCom/Jellyfin.Plugin.OpenSubtitlesCom/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.OpenSubtitlesCom/Jellyfin.Plugin.OpenSubtitlesCom/Configuration/PluginConfiguration.cs
@@ -8,7 +8,7 @@ namespace Jellyfin.Plugin.OpenSubtitlesCom.Configuration
 
         public string ApiKey { get; set; }
         public string Username { get; set; }
-        public string Password { internal get; set; }
+        public string Password { get; set; }
 
         public PluginConfiguration()
         {
